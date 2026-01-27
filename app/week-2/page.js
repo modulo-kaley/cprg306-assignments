6f012a458784bffaf4f22d2cfba64c9506954882
Kaley <kaleyywood@gmail.com>
2026-01-26 19:02:32 -0700

    week 2/page - structure - no styling

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+import Student from "./StudentInfo";
+
 export default function Page() {
     return ( 
-    <main>
-        <h1>Shopping List</h1>
+    <main className="">
+        <h1 className="">Viewing List</h1>
+        <Student/>
     </main>
     );
 }
