1ab86454e3aead3f14e9da0c8dd590fd2a98e1a0
Kaley <kaleyywood@gmail.com>
2026-01-26 19:02:04 -0700

    StudentInfo component structure - no styling

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,13 @@
+import Link from "/.nextLink/";
+
+export default function StudentInfo() {
+    return (
+        <section className="">
+            <h2 className="">Kaley Wood</h2>
+            <p>
+                <Link className="" href="https://cprg306-assignments-olive-beta.vercel.app/">Click to Enter my Repository</Link>
+            </p>
+            <Link/>
+        </section> 
+    );
+}
