f630c05f591866345d2160c30a469a6b657539fc
Kaley <kaleyywood@gmail.com>
2026-01-30 11:32:19 -0700

    swap out for li (I'm becoming article obsessed)

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -4,11 +4,11 @@
 */
 export default function GroceryItem({name, quantity, category}){
     return(
-      <article className="min-w-72 rounded-full border-double text-center text-stone-200 border-stone-600 p-4 shadow-cyan-800 bg-blue-300">
+      <li className="min-w-72 rounded-full border-double text-center text-stone-200 border-stone-600 p-4 shadow-cyan-800 bg-blue-300">
         <h3 className="text-2xl m-4"> 
             {name}
         </h3>
         <p className="justify-center mb-2">{quantity} {category}</p>
-       </article>
+       </li>
     );
 }
