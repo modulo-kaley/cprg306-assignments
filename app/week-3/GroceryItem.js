e67c6d9e0634a5b2408d8ad0104ef0face410c44
Kaley <kaleyywood@gmail.com>
2026-01-30 10:35:32 -0700

    Playing around w/list styling

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -4,11 +4,11 @@
 */
 export default function GroceryItem({name, quantity, category}){
     return(
-      <article className="inline-flex justify-items-normal shrink-14 rounded-full border text-stone-200 border-gray-600">
-        <h3> 
+      <article className="inline-flex justify-items-normal shrink-14 rounded-full border text-stone-200 border-gray-600 p-4 rounded-lg mx-8 max-w-fit">
+        <h3 className="m-4"> 
             {name}
         </h3>
-        <p>{quantity} {category}</p>
+        <p className="text-2xl mb-2">{quantity} {category}</p>
        </article>
     );
 }
