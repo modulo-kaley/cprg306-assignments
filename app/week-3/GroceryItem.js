de7e33bc03c5074c224b0392a49a58b95c5b5929
Kaley <kaleyywood@gmail.com>
2026-01-30 10:10:54 -0700

    Item component

---
+++ app/week-3/GroceryItem.js
@@ -0,0 +1,14 @@
+
+/*If you USE it, you IMPORT it.
+  If you MAKE it, you EXPORT it. 
+*/
+export default function Item({name, quantity, category}){
+    return(
+      <article className="inline-flex justify-items-normal shrink-14 rounded-full border text-stone-200 border-gray-600">
+        <h3> 
+            {name}
+        </h3>
+        <p>{quantity}{category}</p>
+       </article>
+    );
+}
