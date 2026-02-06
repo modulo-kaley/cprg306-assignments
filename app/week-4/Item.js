4e852b6edfb0b637915c251c9a5af30456e72a61
Kaley <kaleyywood@gmail.com>
2026-02-06 09:56:30 -0700

    item level styling

--- app/week-4/Item.js
+++ app/week-4/Item.js
@@ -1,8 +1,8 @@
 export default function Item(name, quantity, category){
     return (
-        <li>
-            <h3 className="">{name}</h3>
-            <p className="">{quantity} {category}</p>
+        <li className="min-w-72 rounded-full border-double text-center text-stone-200 border-stone-600 p-4 shadow-cyan-800 bg-blue-300">
+            <h3 className="text-2xl m-4">{name}</h3>
+            <p className="justify-center mb-2">{quantity} {category}</p>
         </li>
 
     );
