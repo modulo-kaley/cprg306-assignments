36ed32a12eed0d4cc272bad9544ed30ea69ff1f3
Kaley <kaleyywood@gmail.com>
2026-02-16 22:29:31 -0700

    home page styling update

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-  <section className="flex min-h-screen flex-col items-center justify-center bg-purple-300 font-sans text-gray-600">
+  <section className="items-center justify-center bg-purple-300 font-sans text-gray-600 grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 p-4">
       <h1 className="font-extrabold justify-items-normal">Web Development 2 - Assignments</h1>
       <p>
         <Link className="inline-flex justify-items-normal shrink-14 rounded-full border border-gray-600" href="/week-2">Week 2 Assignment</Link>
