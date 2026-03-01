11d67187a62a3a6313e96bd4e6bdc16d1a464d9f
Kaley <kaleyywood@gmail.com>
2026-02-28 22:52:34 -0700

    week-6 page: itemsData - handling - rendering

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,11 +1,19 @@
 import NewItem from "./NewItem";
+import ItemList from "./ItemList";
+import itemsData from "./items.json";
+import { useState } from "react"; 
 
 export default function Page(){
+    const [items, setItems] = useState(itemsData);
+    const handleAddItem = (newItem) => {
+        setItems([...items, newItem]);
+    };
 return(
     <main className="min-h-screen flex flex-col items-center justify-center p-8 bg-gray-950 text-white">
         <section className="w-full max-w-lg">
-            <h1 className="text-4xl font-bold mb-8 tracking-tight text-center">Add a New Item</h1>
-            <NewItem/>
+            <h1 className="text-4xl font-bold mb-8 tracking-tight text-center">Shopping List</h1>
+            <NewItem onAddItem={handleAddItem}/>
+            <ItemList items={items}/>
         </section>
     </main>
 );
